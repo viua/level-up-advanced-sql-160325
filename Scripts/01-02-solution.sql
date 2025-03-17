aab43217cac0478409a1e8a48baad8a201ee1fef
sysfs <vlad.ivashevich@gmail.com>
2025-03-17 22:23:55 +0000

    1.2# Find sales people who have zero sales

---
+++ Scripts/01-02-solution.sql
@@ -0,0 +1,13 @@
+--- Find sales people who have zero sales ---
+
+select e.employeeId, s.salesId
+ FROM employee e
+ LEFT JOIN sales s ON s.employeeId=e.employeeId
+WHERE e.title == 'Sales Person' AND s.salesId is NULL ;
+
+-- select * from sales order by employeeId;
+
+-- select e.employeeId, e.firstName || ' ' || e.lastName AS Name, e.title from employee e where e.title == 'Sales Person' AND e.employeeId not in (select employeeId from sales group by employeeId)
+-- employeeId	Name	     title
+-- 103	      Bob Smith	 Sales Person
+-- 104	      Carla Owen Sales Person
